2588d495ce8e25b57f26ae0c43ee24b0cdca2986
Sarwar <2plus7sarwar@gmail.com>
2023-06-11 00:40:36 +0600

    added react icons and fix responsiveness of footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,24 @@
-
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+import {Link} from 'react-router-dom';
 
 const Footer = () => {
     return (
         <div className='bg-dark text-light md:p-8 p-3'>
-            <div className='flex max-w-[1480px] mx-auto justify-between'>
+            <div className='md:flex max-sm:text-center max-w-[1480px] mx-auto md:justify-between'>
                 <div>
                     <h1 className='text-xl uppercase mb-5'>Sports Camp</h1>
                     <p>232 Park road, CA</p>
                     <p>contact@sportscamp.com</p>
                     <p>+621 999 000</p>
                 </div>
-                <div>
+                <div className="max-sm:mt-10">
                     <h1 className='text-xl uppercase mb-5'>Follow us</h1>
                     <p>Follow us on social media</p>
-                    icons
+                    <div className="flex space-x-5 mt-3 max-sm:justify-center">
+                        <Link><FaFacebookF className="hover:text-light/60 text-xl"></FaFacebookF></Link>
+                        <Link><FaInstagram className="hover:text-light/60 text-xl"></FaInstagram></Link>
+                        <Link><FaTwitter className="hover:text-light/60 text-xl"></FaTwitter></Link>
+                    </div>
                 </div>
             </div>
             <hr className="my-5 max-w-[1480px] mx-auto"/>
