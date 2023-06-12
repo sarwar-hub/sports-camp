b5f0b3ab29c83c013c75aaa73f0fd9c6de7ded35
Sarwar <2plus7sarwar@gmail.com>
2023-06-12 12:40:39 +0600

    modified useSelected hook to find selected item data

--- src/hooks/useSelected.jsx
+++ src/hooks/useSelected.jsx
@@ -1,17 +1,30 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthProvider";
+import useCourses from "./useCourses";
 
 const useSelected = () => {
     // data from context
     const {user} = useContext(AuthContext);
+    
+    const [selected, setSelected] = useState([]);
+    const selectedItems = [];
 
-    // selected items state
-    const [selectedItems, setSelectedItems] = useState([]);
+    // get all courses
+    const [allCourses, ] = useCourses();
+    
+        for (const courseItem of selected) {
+
+            const matchedItem = allCourses.find(course => course._id === courseItem.courseId);
+
+            selectedItems.push(matchedItem);
+        }
+    
+
 
     useEffect(()=>{
         fetch(`http://localhost:5000/selectedItems?email=${user.email}`)
         .then(res=>res.json())
-        .then(data=> setSelectedItems(data))
+        .then(data=> setSelected(data))
         .catch(err=>{
             console.log(err.message);
         })
