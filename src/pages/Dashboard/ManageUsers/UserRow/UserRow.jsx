26082114604e214b08711e0128d7e1b2d3fe7f53
Sarwar <2plus7sarwar@gmail.com>
2023-06-13 10:19:28 +0600

    added make instructor func

--- src/pages/Dashboard/ManageUsers/UserRow/UserRow.jsx
+++ src/pages/Dashboard/ManageUsers/UserRow/UserRow.jsx
@@ -1,6 +1,45 @@
+import Swal from "sweetalert2";
 
 
-const UserRow = ({user, index}) => {
+const UserRow = ({ user, index }) => {
+
+
+    // make instructor
+    const makeInstructor = () => {
+        fetch(`http://localhost:5000/makeInstructor/${user._id}`, {
+            method: 'PATCH',
+            headers: { 'content-type': 'application/json' }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    Swal.fire({
+                        position: 'top-center',
+                        icon: 'success',
+                        title: `${user.name} is Instructor Now !`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                } else if (data.modifiedCount == 0) {
+                    Swal.fire({
+                        position: 'top-center',
+                        icon: 'info',
+                        title: `${user.name} Already is an Instructor !`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
+    }
+
+    // make admin
+    const makeAdmin = () => {
+
+    }
+
     return (
         <tr className='text-gray'>
             <th>{index + 1}</th>
@@ -8,8 +47,8 @@ const UserRow = ({user, index}) => {
             <td>{user.email}</td>
             <td className="capitalize">{user.role}</td>
             <td className="space-x-2">
-               <button className="bg-dark p-1 rounded-none text-light capitalize">Make Instrustor</button>
-               <button className="bg-dark2 p-1 rounded-none text-light capitalize">Make admin</button>
+                <button onClick={makeInstructor} className="bg-dark p-1 rounded-none text-light capitalize">Make Instrustor</button>
+                <button onClick={makeAdmin} className="bg-dark2 p-1 rounded-none text-light capitalize">Make admin</button>
             </td>
         </tr>
     );
